2eeb94d51f325694023f47d73cc174f6ab7988c6
Adrian Eguez <eadepto@hotmail.com>
2018-06-23 09:43:01 -0500

    xml http reqeust

--- app/js/funciones.js
+++ app/js/funciones.js
@@ -48,3 +48,14 @@ function crearContenido() {
     // meter el contenido html en el contenedor
     etiquetaContenedora.appendChild(etiquetaCarta);
 }
+function traerPost(idPost) {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4
+            && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+    xhttp.open("GET", "https://jsonplaceholder.typicode.com/posts/" + idPost, true);
+    xhttp.send();
+}
--- app/js/funciones.ts
+++ app/js/funciones.ts
@@ -62,3 +62,23 @@ function crearContenido() {
 
 
 }
+
+function traerPost(idPost: number) {
+    const xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4
+            && this.status == 200) {
+            console.log(this.responseText);
+        }
+    };
+
+    xhttp.open(
+        "GET",
+        "https://jsonplaceholder.typicode.com/posts/" + idPost,
+        true);
+
+    xhttp.send();
+
+}
+
